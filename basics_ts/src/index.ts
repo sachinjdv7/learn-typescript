0950829355aa05d396b5e0cbbc97f04429b57695
Sachin Jadhav <sachinjdv7@gmail.com>
2025-04-29 02:05:53 +0530

    functions with parm

--- basics_ts/src/index.ts
+++ basics_ts/src/index.ts
@@ -38,3 +38,13 @@ const enum Size {
 let mySize: Size = Size.Large;
 
 console.log(mySize);
+
+// functions
+
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022) return income * 1.2;
+
+  return income * 1.3;
+}
+
+calculateTax(10_000);
