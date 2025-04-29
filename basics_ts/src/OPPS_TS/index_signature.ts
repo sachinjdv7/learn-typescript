73b1e9d7a4b060050d128a4c5659322c991a2826
Sachin Jadhav <sachinjdv7@gmail.com>
2025-04-29 17:54:30 +0530

    learn signature indexing

---
+++ basics_ts/src/OPPS_TS/index_signature.ts
@@ -0,0 +1,10 @@
+class SeatAssignment {
+  // A1 , A2 ...
+  // 'sachin ', 'nitin'
+
+  [seatNumber: string]: string;
+}
+
+const seats = new SeatAssignment();
+seats.A1 = "sachin";
+seats["A2"] = "rohit";
