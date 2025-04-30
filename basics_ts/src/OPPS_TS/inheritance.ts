288c740af701a3da5329f019a242790a9beb85ea
Sachin Jadhav <sachinjdv7@gmail.com>
2025-04-30 12:33:36 +0530

    learn polymorphism

--- basics_ts/src/OPPS_TS/inheritance.ts
+++ basics_ts/src/OPPS_TS/inheritance.ts
@@ -28,10 +28,32 @@ class Teacher extends Person {
   }
 }
 
+class Principle extends Person {
+  override get fullName() {
+    return "Principle " + super.fullName;
+  }
+}
+
 // let student = new Student(1, "sachin", "jadhav");
 // student.takeTest();
 // console.log(student.fullName);
 // console.log(student);
 
-let teacher = new Teacher("piyush", "garg");
-console.log(teacher.fullName);
+// let teacher = new Teacher("piyush", "garg");
+// console.log(teacher.fullName);
+
+// polimorphism -> object can take many forms
+// open clsoed principle - 'classes should be open for extesion and
+//  closed for modification'
+
+getNames([
+  new Student(1, "sachin", "jadhav"),
+  new Teacher("piyush", "garg"),
+  new Principle("hitesh", "chaudhari"),
+]);
+
+function getNames(people: Person[]) {
+  for (let person of people) {
+    console.log(person.fullName);
+  }
+}
