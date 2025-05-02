9d804f7acfea8d8ae650cceb26a9e62539e8bda4
Sachin Jadhav <sachinjdv7@gmail.com>
2025-05-02 19:51:13 +0530

    learn how to work js along ts

---
+++ basics_ts/src/modules/index.ts
@@ -0,0 +1,4 @@
+import { calculateTax } from "./tax";
+
+let tax = calculateTax(100);
+console.log(tax);
---
+++ basics_ts/src/modules/tax.js
@@ -0,0 +1,9 @@
+/**
+ * Calculate income tax
+ * @param {number} income - net salary after income
+ * @returns {number}
+ */
+
+export function calculateTax(income) {
+  return income * 0.3;
+}
